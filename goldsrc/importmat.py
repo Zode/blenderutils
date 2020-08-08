a8a40f3808fd958442524aa51337d11172bb8fd4
Zode <jonzqzode@gmail.com>
2020-08-08 05:01:06 +0300

    chrome node stuff

--- goldsrc/importmat.py
+++ goldsrc/importmat.py
@@ -13,7 +13,7 @@ def FixImportMaterials():
 		if not mat.use_nodes:
 			mat.use_nodes = True
 		
-		node = FindOrMakeNode(mat.node_tree.nodes, "ShaderNodeTexImage", (-300.0, 218.0))
+		texturenode = FindOrMakeNode(mat.node_tree.nodes, "ShaderNodeTexImage", (-300.0, 218.0))
 		
 		texturepath = f"//{mat.name}"
 		try:
@@ -23,9 +23,31 @@ def FixImportMaterials():
 			print(f"Can't load image: {texturepath}!\n{e}")
 			return False
 		
-		node.image = texture
+		texturenode.image = texture
 		
 		#not using a fancy find function here since we just assume Principled BSDF always exists :P
-		mat.node_tree.links.new(node.outputs[0], mat.node_tree.nodes.get("Principled BSDF").inputs[0])
+		bsdf = mat.node_tree.nodes.get("Principled BSDF")
+		mat.node_tree.links.new(texturenode.outputs["Color"], bsdf.inputs["Base Color"])
+		
+		#goldsrc/sven doesn't have fancy shaders... yet
+		bsdf.inputs["Specular"].default_value = 0
+		bsdf.inputs["Metallic"].default_value = 0
+
+		if "chrome" in texturepath.lower():
+			geometrynode = FindOrMakeNode(mat.node_tree.nodes, "ShaderNodeNewGeometry", (-942.0, 218.0))
+			vectornode = FindOrMakeNode(mat.node_tree.nodes, "ShaderNodeVectorTransform", (-729.0, 218.0))
+			mappingnode = FindOrMakeNode(mat.node_tree.nodes, "ShaderNodeMapping", (-516.0, 218.0))
+			mat.node_tree.links.new(geometrynode.outputs["Normal"], vectornode.inputs["Vector"])
+			mat.node_tree.links.new(vectornode.outputs["Vector"], mappingnode.inputs["Vector"])
+			mat.node_tree.links.new(mappingnode.outputs["Vector"], texturenode.inputs["Vector"])
+			
+			vectornode.vector_type = "VECTOR"
+			vectornode.convert_from = "OBJECT"
+			vectornode.convert_to = "CAMERA"
+			mappingnode.vector_type = "POINT"
+			mappingnode.inputs["Location"].default_value[0] = 0.5
+			mappingnode.inputs["Location"].default_value[1] = 0.5
+			mappingnode.inputs["Scale"].default_value[1] = 0.5
+			mappingnode.inputs["Scale"].default_value[2] = 0.5
 		
 	return True
