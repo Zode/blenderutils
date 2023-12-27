5067a943c98099ea2e84bd5d624607f166cb52a1
Zode <jonzqzode@gmail.com>
2023-12-27 02:21:12 +0200

    add extra keybind to ble able to toggle between custom weight paint gradient

--- __init__.py
+++ __init__.py
@@ -55,18 +55,45 @@ class ZODEUTILS_PREFERENCES(bpy.types.AddonPreferences):
 	def draw(self, context):
 		addon_updater_ops.update_settings_ui(self, context)
 	
+class ZODEUTILS_WeightViewToggle(bpy.types.Operator):
+	"""Vertex weight gradient toggler"""
+	bl_idname = "zodeutils.magicview"
+	bl_label = "vertex weight gradient toggler"
+
+	def execute(self, context):
+		bpy.context.preferences.view.use_weight_color_range = not bpy.context.preferences.view.use_weight_color_range
+		
+		return {"FINISHED"}
+
+global_addon_keymaps = []
 
 def register():
 	addon_updater_ops.register(bl_info)
 	bpy.utils.register_class(ZODEUTILS_PREFERENCES)
 	material_gui.register()
 	bake_gui.register()
+	bpy.utils.register_class(ZODEUTILS_WeightViewToggle)
+
+	window_manager = bpy.context.window_manager
+	if window_manager.keyconfigs.addon:
+		keymap = window_manager.keyconfigs.addon.keymaps.new(name="3D View", space_type="VIEW_3D")
+		keymap_item = keymap.keymap_items.new(ZODEUTILS_WeightViewToggle.bl_idname, "Q", "PRESS", shift=True, ctrl=True)
+
+		global_addon_keymaps.append((keymap, keymap_item))
 
 def unregister():
 	addon_updater_ops.unregister()
 	bpy.utils.unregister_class(ZODEUTILS_PREFERENCES)
 	material_gui.unregister()
 	bake_gui.unregister()
+	bpy.utils.unregister_class(ZODEUTILS_WeightViewToggle)
+
+	window_manager = bpy.context.window_manager
+	if window_manager and window_manager.keyconfigs and window_manager.keyconfigs.addon:
+		for keymap, keymap_item in global_addon_keymaps:
+			keymap.keymap_items.remove(keymap_item)
+		
+	global_addon_keymaps.clear()
 
 if __name__ == "__main__":
 	register()
