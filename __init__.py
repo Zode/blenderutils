821c8c195684b5db560c9efd90f9b3f9ec987c87
Zode <jonzqzode@gmail.com>
2025-10-11 10:53:46 +0300

    Bump semantic

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ from .vertexbone import *
 bl_info = {
 	"name" : "Zode's blender utils",
 	"author" : "Zode",
-	"version" : (1, 4, 4),
+	"version" : (1, 4, 5),
 	"blender" : (4, 4, 0),
 	"description" : "Adds various utility function(s) to blender",
 	"warning": "",
